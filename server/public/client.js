8af3c48c7c6a9aa6822d85a956e54177e87c41e3
Johnny Packard <packard81@me.com>
2018-05-23 17:30:00 -0500

    finished main and half of stretch

---
+++ server/public/client.js
@@ -0,0 +1,44 @@
+$( document ).ready( readyNow );
+
+function readyNow(){
+    getQuotes();
+    $( '#addQuoteButton').on( 'click', addQuote );
+    $( '#addNameButton').on( 'click', addName );
+}
+
+function addQuote(){
+    console.log( 'in addQuote' );
+    let objectToSend = {
+        quote: $( '#quoteIn' ).val(),
+        name: $( '#nameIn' ).val()
+    }; // end addQuoteName
+    console.log( 'sending', objectToSend );
+    $.ajax({
+        method: 'POST',
+        url: '/quotes',
+        data: objectToSend
+    }).then( function( response ){
+        console.log('back from server with:', response );
+        getQuotes();
+        
+    }); // end Hey Ajax!
+} // end addQuote
+
+function displayQuotes( quote ){
+    let el = $( '#quoteOut');
+    el.empty();
+    for (quote of quotes){
+        el.append( '<li> + quote </li>');
+    } // end quote for loop
+}
+
+function getQuotes(){
+    console.log( 'in getQuotes' );
+    $.ajax({
+        method: 'GET',
+        url: '/quotes'
+    }).then( function( response ){
+        console.log( 'back from server with:', response );
+        displayQuotes( response );
+    });
+}
---
+++ server/server.js
@@ -0,0 +1,29 @@
+// declaring these are what's required to make the code work in server
+let express = require('express');
+let app = express();
+let bodyParser = require('body-parser');
+
+// declare the globals we need (i.e. port and output array)
+const port = 5000;
+let quotes = [];
+
+// below is what the server/app will use for communicating (handshake)
+app.use( express.static( 'server/public' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+// spin up the server
+app.listen( port, ()=>{
+    console.log( 'server up on:', port );
+}); // end server spin
+
+// get the route of the request and response
+app.get( '/quotes', ( req, res )=>{
+    console.log( 'in GET hit for /quotes route:', req.body );
+    res.send( quotes );
+}); // end GET
+
+// post output
+app.post( '/quotes', (req, res)=>{
+    console.log( 'in POST')
+    quotes.push( req.body.quote.name );
+    res.sendStatus( 200 );
+}); // end POST
